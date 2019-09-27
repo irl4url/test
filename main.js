5932bb994cf8a09a54f91552f77704ccb76dec70
irl4url <irl4url@gmail.com>
2019-09-26 21:31:14 -0400

    updated

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-var Human = function(name, hair){
+function Human (name, hair, pants){
     this.name = name;
     this.hair = hair;
-    this.pants = true;
+    this.pants = pants;
 
 }
 
-var tom = new Human(tom, brown);
-
-console.log (tom)
+var tom = new Human("tom", "brown", "red");
+var sarah = new Human ("sarah", "blonde", "blue")
+console.log(sarah)
+console.log(tom);
